a18ebb44c16097d444686b9394cc2212696610ae
hackezar <107069171+hackezar@users.noreply.github.com>
2024-06-25 22:05:08 -0400

    Added event listeners for all the contact divs
    Phone and email will copy the designated info
    to your clipboard
    
    Clicking the github div will take you to the
    profile

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -18,4 +18,41 @@ export default function addEventListeners() {
         let about = document.getElementById('about-container');
         about.scrollIntoView({behavior: 'smooth', block:'end'});
     })
+
+    // Contact Section Listeners
+    // Copy Phone Number to Clipboard
+    let phoneNumber = document.getElementById('phoneContact');
+    phoneNumber.addEventListener('click', () => {
+        // Below will copy the following to the computers clipboard
+        navigator.clipboard.writeText("+1(727)280-5737").then(() => {
+            console.log('Content copied to clipboard');
+            let phoneTitle = document.getElementById('phoneTitle');
+            phoneTitle.innerHTML = 'Copied to Clipboard';
+            // Switches html back to original after 3 seconds
+            setTimeout(function(){phoneTitle.innerHTML = "Phone (Click to Copy)"}, 3000);
+            // Text successfully copied
+        }, () => {
+            console.error('Failed to copy to clipboard');
+            // text failed to copy to clipboard
+        })
+    })
+
+    // Copy Email to Clipboard
+    let emailAddress = document.getElementById('emailContact');
+    emailAddress.addEventListener('click', () => {
+       // Below will copy the following to the computers clipboard
+        navigator.clipboard.writeText("jackhalcisak@outlook.com").then(() => {
+            console.log('Content Copied');
+            let emailTitle = document.getElementById('emailTitle');
+            emailTitle.innerHTML = "Copied to Clipboard";
+            // Switches html back to original after 3 seconds
+            setTimeout(function(){emailTitle.innerHTML = "Email (Click to Copy)"}, 3000)
+        })
+    })
+
+    // Open new tab to Github Profile
+    let githubProfile = document.getElementById('githubProfile');
+    githubProfile.addEventListener('click', () => {
+        window.open("https://github.com/hackezar", "_blank", "noopener noreferrer");
+    })
 }
